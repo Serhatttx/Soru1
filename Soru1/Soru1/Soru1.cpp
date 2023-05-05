ca689227bf3bd88b370efa6fb838ac52375d266a
Serhatttx <132668211+Serhatttx@users.noreply.github.com>
2023-05-05 15:48:19 +0300

    Soru1

---
+++ Soru1/Soru1/Soru1.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+int main() {
+    int dizi[] = { 10, 20, 20, 10, 10, 20, 5, 20 };
+    int boyut = sizeof(dizi) / sizeof(dizi[0]);
+
+    int i, j, sayac, frekans;
+
+    printf("Elemanlar\tFrekanslar\n");
+
+    for (i = 0; i < boyut; i++) {
+        sayac = 1;
+        frekans = 0;
+        for (j = i + 1; j < boyut; j++) {
+            if (dizi[i] == dizi[j]) {
+                sayac++;
+            }
+        }
+        if (sayac > 0) {
+            frekans = sayac;
+            for (j = i + 1; j < boyut; j++) {
+                if (dizi[i] == dizi[j]) {
+                    dizi[j] = -1; 
+                }
+            }
+            printf("%d\t\t%d\n", dizi[i], frekans);
+        }
+    }
+
+    return 0;
+}
